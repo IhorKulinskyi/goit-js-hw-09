62647450be4cfc65f4d69e792d23efb542ae542d
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2023-04-10 13:09:13 +0300

    task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,26 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+  };
+  
+  refs.startBtn.addEventListener('click', changeColor);
+  refs.stopBtn.addEventListener('click', stopChangeColor);
+  
+  function changeColor() {
+    timerId = setInterval(() => {
+      refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    refs.startBtn.disabled = true;
+  }
+  
+  function stopChangeColor() {
+    clearInterval(timerId);
+    refs.startBtn.disabled = false;
+  }
+  
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
